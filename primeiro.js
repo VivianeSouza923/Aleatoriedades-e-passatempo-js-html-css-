637a287ad11eaae0d1c1939cd216816500492092
Viviane Souza <70662490+VivianeSouza923@users.noreply.github.com>
2021-03-16 22:43:59 -0300

    Add files via upload

---
+++ primeiro.js
@@ -0,0 +1,36 @@
+// assim se comenta o código em uma linha
+
+/*assim
+se
+comenta
+mais de
+uma
+linha de uma vez*/
+
+/* como resolver problemas no acento? 
+usa o: < meta charset = "utf-8" > */
+
+
+//  ****** imprimir em tela
+
+/* 
+
+var mensagem = "Deus é bondoso!";
+alert(mensagem);
+
+*/
+
+// ****** operações matemáticas: fazer e imprimir
+
+var a = 2;
+var b = 3;
+
+var soma = a + b;
+var sub = a - b;
+var mult = a * b;
+var div = a / b;
+
+alert(soma);
+alert(sub);
+alert(mult);
+alert(div);
