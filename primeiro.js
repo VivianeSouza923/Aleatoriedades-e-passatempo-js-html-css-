4810a2cb7d8e20ef02a91e527eca4a3e1e982ee1
Viviane Souza <70662490+VivianeSouza923@users.noreply.github.com>
2021-04-29 22:01:15 -0300

    Add files via upload
    update

--- primeiro.js
+++ primeiro.js
@@ -21,16 +21,79 @@ alert(mensagem);
 */
 
 // ****** operações matemáticas: fazer e imprimir
-
+/*
+// declaração de variáveis
 var a = 2;
 var b = 3;
 
+// executar as operações
 var soma = a + b;
 var sub = a - b;
 var mult = a * b;
 var div = a / b;
 
+// comando alert exibe uma mensagem.
 alert(soma);
 alert(sub);
 alert(mult);
-alert(div);
+alert(div); */
+
+//estruturas condicionais
+
+// verificando
+//alert(5 % 3);
+
+var b = 2;
+var c = "vivi";
+
+/*
+if (b % 2 == 1) {
+    alert("Número ímpar");
+} else {
+    alert("Número par");
+}*/
+/*
+if (c % 2 == 0) {
+    alert("Número par");
+} else if (c % 2 == 1) {
+    alert("Número ímpar");
+
+} else {
+    alert("Acredito que você não tenha digitado um número");
+}*/
+
+// estruturas de repetição
+
+// estrutura de condição while
+/*
+var i = 0;
+
+while (i < 3) {
+    alert(i);
+    i = i + 1;
+}
+*/
+// laço for
+/*
+var j;
+
+for (j = 0; j < 2; j++) {
+    alert(j);
+}*/
+
+// tipos de dados em JS
+
+var numero = 6;
+var decimal = 4.5;
+var string = "vivi";
+var lista = ["vivi", "laranja", "ninho"];
+
+// alert(string);
+
+// arrays
+
+//alert(lista[2]);
+
+for (i in lista) {
+    alert(lista[i]);
+}
