c2c3e583aaace424d440ed7ab7f9c2859383a9c1
Viviane Souza <70662490+VivianeSouza923@users.noreply.github.com>
2021-05-03 15:50:22 -0300

    Add files via upload
    update

---
+++ script_jquery.js
@@ -0,0 +1,10 @@
+//$(selector).ação();
+
+// o docmento já está pronto?
+$(document).ready(function() {
+    //o clique no botão
+    $('button').click(function() {
+        //vai esconder o que eu coloquei no h1
+        $('h1').hide();
+    });
+});
