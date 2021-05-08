ff6b6554189af9ef5018e60a536c78345f57bc97
Viviane Souza <70662490+VivianeSouza923@users.noreply.github.com>
2021-05-08 18:43:54 -0300

    Add files via upload
    update

--- script_jquery.js
+++ script_jquery.js
@@ -34,6 +34,7 @@ $(function() {
         $('#yh').css("color", "green");
     });
 }); */
+/*
 $(function() {
     $('#green').click(function() {
         $('p').css("color", "green");
@@ -51,5 +52,68 @@ $(function() {
         $('p').delay(3000);
         // se eu quiser um reaparecimento mais suave posso colocar 'slow' dentro do fadeIn
         $('p').fadeIn('slow')
+    });
+}); */
+/*
+$(function() {
+    $('#green').click(function() {
+        $('p').css("color", "green");
+        //o fadeout vai fazer desaparecer as mensagens contidas na div <p>
+        $('p').fadeOut();
+        // vou aguardar 3 segundos
+        $('p').delay(3000);
+        //depois de esperar os 3 sehgundos, vou fazer reaparecer com o fadeIn
+        $('p').fadeIn();
+    })
+    $('#pink').click(function() {
+        $('p').css("background-color", "pink");
+        //o fadeout vai fazer desaparecer as mensagens contidas na div <p>
+        // o fast indica que esse desaparecimento será mais rápido
+        $('p').fadeOut('fast');
+        $('p').delay(3000);
+        // se eu quiser um reaparecimento mais rápida posso colocar 'fast' dentro do fadeIn
+        $('p').fadeIn('fast')
+    });
+}); */
+/*
+$(function() {
+    $('#green').click(function() {
+        $('p').css("color", "green");
+        //o fadeout vai fazer desaparecer as mensagens contidas na div <p>
+        $('p').fadeOut();
+        // vou aguardar 3 segundos
+        $('p').delay(3000);
+        //depois de esperar os 3 sehgundos, vou fazer reaparecer com o fadeIn
+        $('p').fadeIn();
+    })
+    $('#pink').click(function() {
+        $('p').css("background-color", "pink");
+        //o fadeout vai fazer desaparecer as mensagens contidas na div <p>
+        $('p').fadeOut(4000);
+        $('p').delay(3000);
+        // se eu quiser um reaparecimento mais suave posso colocar 'slow' dentro do fadeIn
+        // para este caso agora eu quero passar os segundos que eu quero que apareça
+        $('p').fadeIn(5000)
+    });
+});*/
+
+// gravar mensagens com jquery
+
+$(function() {
+    $('#green').click(function() {
+        $('p').css("background-color", "green");
+        $('p').fadeOut();
+        $('p').delay(1000);
+        $('p').fadeIn();
+    });
+
+    $('#pink').click(function() {
+        $('p').css("background-color", "pink");
+        //$('#mensagem').text("cor alterada com sucesso");
+        //$('#mensagem').css('color', 'pink');
+        //$('#mensagem').css('border', '1px solid pink');
+        //$('#mensagem').fadeOut(3000);
+        // encadeamento dessas linhas para #mensagem em bloco:
+        $('#mensagem').text("cor alterada com sucesso").css('color', 'pink').css('border', '1px solid pink').fadeOut(3000);
     });
 });
